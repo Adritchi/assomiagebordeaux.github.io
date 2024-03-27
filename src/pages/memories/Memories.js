34c576c6228c3a920f8abd0aa0d520e8a4a2a6c0
MadianeG <madiane.gonnel@gmail.com>
2024-03-27 11:25:43 +0100

    Verification estAdmin + affichage Tuiles

--- src/pages/memories/Memories.js
+++ src/pages/memories/Memories.js
@@ -1,18 +1,51 @@
 import React, {Component} from 'react';
 
-import {Tuile} from '../../modules/utilitaires/Tuile';
-
 import MemoriesIllustration from '../../assets/images/memories.jpg';
 
 // WEI 2022
 import WEI20222023_1 from '../../assets/images/memories/2022-2023-wei/WEI2022_1.png';
 
+import ListMemories from '../../modules/memories/GetMemories';
+import CreateMemories from '../../modules/memories/CreateMemories';
+
 import '../../assets/design/pages/memories.css'
 
 class Memories extends Component {
 
-    render() {
+    // Initialisation de l'état local
+    state = {
+        estAdmin: false, 
+        showCreateMemoryForm: false, 
+    };
 
+    componentDidMount() {
+        this.checkLoginStatus(); // Vérifie immédiatement l'état de connexion
+    }
+
+    // Vérifie le statut de connexion
+    checkLoginStatus = async () => {
+        try {
+            // Requête pour vérifier le statut de connexion côté serveur
+            const response = await fetch('/checkLoginStatus');
+            const data = await response.json();
+            // Met à jour l'état estAdmin avec le résultat de la requête
+            this.setState({ estAdmin: data.adminConnected });
+        } catch (error) {
+            // En cas d'erreur, laisse estAdmin sur false
+            this.setState({ estAdmin: false });
+        }
+    };
+
+    // Bascule l'affichage du formulaire de création d'événement
+    toggleCreateMemoryForm = () => {
+        this.setState((prevState) => ({
+            showCreateEventForm: !prevState.showCreateMemoryForm,
+        }));
+    };
+
+    render() {
+        // Extraction de l'état estAdmin du state
+        const { estAdmin } = this.state;
         return (
             <div class="page-memories">
                 <div style={{backgroundImage: "url(" + MemoriesIllustration + ")"}} class="page-memories-illustration">
@@ -24,34 +57,16 @@ class Memories extends Component {
                 </div>
                 <div class="page-memories-content">
                     <div class="page-memories-content-margin">
-
-                        {/* <div class="page-memories-content-one-title">Nouveauté</div> */}
-
+                        <div>
+                            {estAdmin && (
+                                <button onClick={this.toggleCreateMemoryForm}>Créer un souvenir</button>
+                            )}
+                            {this.state.showCreateEventForm && <CreateMemories />}
+                        </div>
                         <div className="page-memories-content-one-title">2022/2023</div>
-                        <section className="page-memories-content-tuiles">
-                            <Tuile
-                                display="true"
-                                type="memories"
-                                // siVideo="true" // dégage
-                                // nombreImages="1" // dégage
-                                img1={WEI20222023_1} // dégage
-                                // img2={WEI20222023_2} // dégage
-                                // img3={WEI20222023_3} // dégage
-                                // img4={WEI20222023_4} // dégage
-                                // img5={WEI20222023_5} // dégage
-                                titreGauche="MIAGE WEI-LANTA" // Titre principal 
-                                dateMemories="1-2 oct." // date (+ année) | En fonction de l'année -> Trie
-                                description=" Si toi aussi tu es un vrai aventurier, alors arbore ton plus beau bandana et sois le dernier à rester debout dans ce WEI-LANTA !"
-                                lien="https://photos.app.goo.gl/ZTiHth3f8XSvUjgv5"
-                                siNouveau="true"
-                                // siDateDeDispo="false" // dégage
-                                // jourDispo="10" // dégage
-                                // moisDispo="octobre" // dégage
-                                // anneeDispo="2022" // dégage
-                                // siPremierListe="true" // En fonction de la date (pas pertinent)
-                            />
-                        </section>
-
+                            <section className="page-memories-content-tuiles">
+                                <ListMemories estAdmin={estAdmin}/>
+                            </section>
                         <div class="page-memories-content-title">2021/2022</div>
                         <section class="page-memories-content-tuiles">
                         </section>
