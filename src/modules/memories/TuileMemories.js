29d59f559499af324b0b6c1ee1fa87507c77375d
MadianeG <madiane.gonnel@gmail.com>
2024-03-27 11:26:34 +0100

    Ajout des informations du souvenir dans la tuile

--- src/modules/memories/TuileMemories.js
+++ src/modules/memories/TuileMemories.js
@@ -7,91 +7,26 @@ import '../../assets/design/commun/tuileMemories.css';
 
 export function TuileMemories(props) {
 
-    var numberImage = Math.floor(Math.random() * 5);
-    console.log("number : " + numberImage);
-    let image = "url(" + props.img1 + ")";
-
-    var marginLeftTuileMemories = '0px';
-    if (props.siPremierListe === "false") {
-        marginLeftTuileMemories = '20px';
-    }
-
-    switch (numberImage) {
-        case 0:
-            image = "url(" + props.img1 + ")";
-            break;
-        case 1:
-            if (props.nombreImages >= 2) {
-                image = "url(" + props.img2 + ")";
-            }
-            break;
-        case 2:
-            if (props.nombreImages >= 3) {
-                image = "url(" + props.img3 + ")";
-            }
-            break;
-        case 3:
-            if (props.nombreImages >= 4) {
-                image = "url(" + props.img4 + ")";
-            }
-            break;
-        case 4:
-            if (props.nombreImages == 5) {
-                image = "url(" + props.img5 + ")";
-            }
-            break;
-        default:
-            image = "url(" + props.img1 + ")";
-            break;
-    }
-
-    // const dateDispoAvecJour = "Disponible le " + props.jourDispo + " " + props.moisDispo + " " + props.anneeDispo + ".";
-    // const dateDispoSansJour = "Disponible en " + props.moisDispo + " " + props.anneeDispo + ".";
-
-    if (props.siDateDeDispo === "true") {
-        // return (
-        //     <div class="module-tuileMemories" style={{marginLeft: marginLeftTuileMemories}}>
-        //         <div style={{backgroundImage: image}} class="module-tuileMemories-image">
-        //         </div>
-        //         <div class="module-tuileMemories-titres">
-        //             <div class="module-tuileMemories-titreGauche">
-        //                 {props.titreGauche}
-        //             </div>
-        //             <div class="module-tuileMemories-titreDroite">
-        //                 {props.titreDroite}
-        //             </div>
-        //         </div>
-        //         <div class="module-tuilesMemories-dateDispo">
-        //             {props.jourDispo === ""
-        //                 ?
-        //                 dateDispoSansJour
-        //                 :
-        //                 dateDispoAvecJour
-        //             }
-        //         </div>
-        //     </div>
-        // );
-    } else {
         return (
-            <div class="module-tuileMemories" style={{marginLeft: marginLeftTuileMemories}}>
+            <div class="module-tuileMemories" style={{marginLeft: '0px'}}>
                 <a href={props.lien} target="_blank" rel="noopener noreferrer nofollow"
                    style={{textDecoration: 'none'}}>
-                    <div style={{backgroundImage: image}} class="module-tuileMemories-image">
-                        {props.siNouveau === "true"
+                    <img src={props.image} class="module-tuileMemories-image">
+                        {/* {props.siNouveau === "true"
                             ?
                             <div class="module-tuileMemories-nouveau">Nouveau</div>
                             :
                             <div></div>
-                        }
-                    </div>
+                        } */}
+                    </img>
                 </a>
                 <div class="module-tuileMemories-titre">
                 {/* <div class="module-tuileMemories-titreGauche"> */}
-                    {props.titreGauche}
+                    {props.titre}
                 </div>
 
                 <div class="module-tuileMemories-dateMemories">
-                    {props.dateMemories}
+                    {props.date}
                 </div>
                 {/* </div> */}
         
@@ -100,7 +35,6 @@ export function TuileMemories(props) {
                 </div>
             </div>
         );
-    }
 }
 
 export default TuileMemories;
